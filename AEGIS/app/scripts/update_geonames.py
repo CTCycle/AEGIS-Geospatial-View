4870c4bce664dae1d49afcaabce5ad3350e01785
CTCycle <thomas.virdis89@gmail.com>
2025-10-29 00:07:19 +0100

    updated gitignore

--- AEGIS/app/scripts/update_geonames.py
+++ AEGIS/app/scripts/update_geonames.py
@@ -3,13 +3,8 @@ from __future__ import annotations
 from AEGIS.app.utils.repository.database import database
 from AEGIS.app.utils.updater import GeonamesUpdater
 
-
-# -----------------------------------------------------------------------------
-def main() -> None:
+###############################################################################
+if __name__ == "__main__":
     database.initialize_database()
     updater = GeonamesUpdater()
     updater.update()
-
-
-if __name__ == "__main__":
-    main()
