f773fd1a1aba6a8be66eb218563f77f68f1b1e07
CTCycle <thomas.virdis89@gmail.com>
2025-12-10 17:31:31 +0100

    removed geonames service and updater

--- AEGIS/server/utils/updater/__init__.py
+++ AEGIS/server/utils/updater/__init__.py
@@ -1,4 +1,1 @@
-from AEGIS.server.utils.updater.geonames import GeonamesUpdater
-from AEGIS.server.utils.updater.gibs import GIBSLayersUpdater
-
-__all__ = ["GeonamesUpdater", "GIBSLayersUpdater"]
+from __future__ import annotations
